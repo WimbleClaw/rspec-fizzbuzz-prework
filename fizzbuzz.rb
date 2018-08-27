7fedae498d9ad2b85fd895f00b1094c7b37186b0
Aisha D'Souza <aishad.souza@yahoo.com>
2018-08-27 13:17:16 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
   if int % 3 == 0 and fizz % 5 == 0 
     puts "fizzbuzz"
-  elsif fizz % 3 == 0 
+  elsif int % 3 == 0 
     "Fizz"
   else
-    fizz % 5 == 0
+    int % 5 == 0
     "Buzz"
   end
 end
